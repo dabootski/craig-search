f5d7bff21b2588132e6fe75aa90eab6f45ff8ae4
dabootski <jasonbucki@gmail.com>
2014-04-18 09:25:47 -0500

    added search result detail page

--- www/app/js/controllers.js
+++ www/app/js/controllers.js
@@ -3,14 +3,36 @@
 /* Controllers */
 
 angular.module('craigSearch.controllers', [])
-  .controller('SearchesController', ['$scope', 'SearchService',
-    function($scope, SearchService) {
+  .controller('SearchesController', ['$scope', '$location', 'SearchService',
+    function($scope, $location, SearchService) {
       $scope.searches = SearchService.savedSearches();
+      $scope.showResults = function(searchId) {
+        $location.path("/searches/" + searchId + "/results");
+      }
     }
   ])
-  .controller('SearchResultsController', ['$scope', '$routeParams', 'SearchService',
-    function($scope, $routeParams, SearchService) {
+  .controller('SearchResultsController', ['$scope', '$location', '$routeParams', 'SearchService', 'SearchResultsService',
+    function($scope, $location, $routeParams, SearchService, SearchResultsService) {
       $scope.search = SearchService.find($routeParams.searchId);
+      $scope.results = SearchResultsService.results($routeParams.searchId);;
+      $scope.showResult = function(searchId, resultId) {
+        $location.path("/searches/" + searchId + "/results/" + resultId);
+      }
+      $scope.back = function() {
+        $location.path("/searches");
+      }
+    }
+  ])
+  .controller('SearchResultDetailController', ['$scope', '$location', '$routeParams', 'SearchService', 'SearchResultsService',
+    function($scope, $location, $routeParams, SearchService, SearchResultsService) {
+      $scope.result = SearchResultsService.find($routeParams.resultId);
+      //$scope.results = SearchResultsService.results($routeParams.searchId);;
+      //$scope.showResult = function(searchId, resultId) {
+        //$location.path("/searches/" + searchId + "/results/" + resultId);
+      //}
+      $scope.back = function() {
+        $location.path("/searches/" + $routeParams.searchId + "/results");
+      }
     }
   ]);
 
--- www/app/js/services.js
+++ www/app/js/services.js
@@ -36,3 +36,30 @@ craigSearchServices.factory('SearchService', [
   }
 ]);
 
+craigSearchServices.factory('SearchResultsService', [
+  function() {
+    var results = [
+      { id: 1, searchId: 1, title: "BMW Steering Wheel Multifunction Complete W/ Air Bag 528i 540i 740i X5", description: "Located in Minneapolis, MN", date: "04/18", price: "$4000"},
+      { id: 2, searchId: 2, title: "2003 BMW m3 convertible 6sp", description: "Located in Eau Claire, WI", date: "04/17", price: "$1000"},
+      { id: 3, searchId: 3, title: "Bmw 325i M3 chassis", description: "Located in Minneapolis, MN", date: "04/13", price: "$40000"},
+      { id: 4, searchId: 4, title: "BMW E90 3 Series 325i 325xi Rear Bumper/Impact Bar - BLACK", description: "Located in Minneapolis, MN", date: "04/08", price: "$24000"},
+    ];
+
+    return {
+      results: function(searchId) {
+        return results;
+      },
+      find: function(resultId) {
+        var found = null;
+
+        angular.forEach(results, function(result) {
+          if(result.id == resultId) {
+            found = result;
+          }
+        });
+
+        return found;
+      }
+    };
+  }
+]);
